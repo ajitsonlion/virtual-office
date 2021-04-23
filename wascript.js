03469043ab8a7357092b43cdbbcd4b77506ef722
Ajit Kumar Singh <ajitsonlion@gmail.com>
2021-04-23 21:31:34 +0200

    teams logo

--- wascript.js
+++ wascript.js
@@ -1,7 +1,6 @@
 const TEAMS_LINK_JAKARTA='https://teams.microsoft.com/l/meetup-join/19%3ameeting_MzJiZjMzOGQtYzgwNy00YjA1LTg4ODEtMjcwYTc3ODg5ZWYx%40thread.v2/0?context=%7b%22Tid%22%3a%22e6dbe219-77ef-4b6a-af83-f9de7de08923%22%2c%22Oid%22%3a%22b71d33a2-274e-42c3-9b8b-701b5da73544%22%7d';
 
 WA.onEnterZone('jakarta', (e) => {
-  console.log('onEnterZone jakarta',e);
   setTimeout(()=>WA.openTab(TEAMS_LINK_JAKARTA),1000);
 
 });
