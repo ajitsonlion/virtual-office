2d7b20020bae97255d244f06770fe08c69b9d616
Anja Ohlhäuser <anja.ohlhaeuser@jambit.com>
2021-06-03 12:26:21 +0200

    Edit welcome-to-jambit map

--- tilesets/ALL.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="ALL" tilewidth="32" tileheight="32" tilecount="20" columns="5">
- <image source="../ALL.png" width="160" height="128"/>
-</tileset>
